4525825d25d9eb7773060d5b2a021cfe4e39118c
Ryan David Sheasby <ryan@sheasby.dev>
2019-08-14 15:17:19 +0200

    Improve emacs config

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -3,27 +3,52 @@
 ;; Place your private configuration here
 (setq-default truncate-lines nil)
 (setq word-wrap t)
+(setq-default select-enable-clipboard nil)
 (evil-escape-mode -1)
 (add-hook! markdown-mode
   (auto-fill-mode -1))
 (after! evil-snipe
   (evil-snipe-mode -1))
 
+(map! :map evil-treemacs-state-map "<ESC>" #'evil-window-next)
+
 (map! :nv ";" #'evil-ex)
-(map! :nvo "-" "$")
+(map! :nvo "-" #'evil-end-of-line)
 (map! :nvo (kbd "C-p") 'treemacs-select-window)
-(map! :map evil-treemacs-State-map "<ESC>" 'evil-window-next)
 (map! :n (kbd "`") (lambda () (interactive)
                      (call-interactively 'evil-invert-char)
-                     (evil-backward-char 1)))
+                     (evil-backward-char 1)
+                     ))
 (map! :v (kbd "`") (lambda () (interactive)
                      (call-interactively 'evil-invert-case)
                      (evil-normal-state)
                      (evil-visual-restore)
                      (if (eq evil-visual-selection 'line)
-                         (evil-forward-char 1))))
+                         (evil-forward-char 1))
+                     ))
+
 (map! :v (kbd "C-/") (lambda () (interactive)
                        (call-interactively 'evil-commentary)
                        (evil-normal-state)
-                       (evil-visual-restore)))
+                       (evil-visual-restore)
+                       ))
 (map! :n (kbd "C-/") 'evil-commentary-line)
+
+(map! :nv (kbd "\\y") (lambda () (interactive)
+                       (evil-use-register ?+)
+                       (set--this-command-keys "y")
+                       (call-interactively 'evil-yank)
+                       ))
+(map! :nv (kbd "\\p") (lambda () (interactive)
+                       (evil-use-register ?+)
+                       (call-interactively 'evil-paste-after)
+                       ))
+(map! :nv (kbd "\\P") (lambda () (interactive)
+                       (evil-use-register ?+)
+                       (call-interactively 'evil-paste-before)
+                       ))
+
+(map! :map org-mode-map
+      :localleader
+      :prefix ("b" . "tables")
+      "d" #'org-table-delete-column)
