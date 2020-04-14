77ed20c8cb18abc0a92c72368bc3171188b2f041
Ryan David Sheasby <ryan@sheasby.dev>
2020-04-14 02:44:33 +0200

    Add snippet for swaggo annotation

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -19,7 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrains Mono" :size 14))
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+(setq doom-big-font (font-spec :family "Fira Code" :size 20))
 
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
@@ -140,3 +141,4 @@
 ;; Fix projectile detection of go modules directory
 (after! projectile
   (add-to-list 'projectile-project-root-files-bottom-up "go.mod"))
+
