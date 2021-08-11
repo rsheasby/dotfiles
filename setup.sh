2414c1ad7408ea7f1ec2a758406bf6e2556d1d57
Ryan David Sheasby <ryan@sheasby.dev>
2021-08-11 10:02:19 +0200

    Prevent recursive symlink when running script twice

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ mkdir -p ~/.vim/backupdir
 ## Setup Lazygit
 mkdir -p ~/Library/Application Support/jesseduffield/
 
+## Delete existing symlinks if they exist
+rm -f ~/.config/nvim
+rm -f ~/.doom.d
+rm -f ~/Library/Application\ Support/jesseduffield/lazygit
+
 ## Link dotfiles directories
 CWD=$(pwd)
 ln -s $CWD/neovim ~/.config/nvim
